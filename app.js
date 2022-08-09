871e1ae6f849a801b557b1b51858c1d1a44472c5
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-09 13:23:18 +0000

    feat: added blocks + player + move function

--- app.js
+++ app.js
@@ -1,13 +1,80 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+const boardWidth = 560
 
+const playerStart = [230, 10]
+let currentPosition = playerStart
 
-//creates blocks
-function addBlock() {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.left = '100px'
-    block.style.bottom = '50px'
-    grid.appendChild(block)
+//creates block individual
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis,yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
 }
 
-addBlock()
+// All blocks
+const blocks = [
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+    new Block(10,240),
+    new Block(120,240),
+    new Block(230,240),
+    new Block(340,240),
+    new Block(450,240),
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210),
+]
+
+//creates blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+    }
+}
+
+addBlocks()
+
+// add player
+const player = document.createElement('div')
+player.classList.add('player')
+drawPlayer()
+grid.appendChild(player)
+
+//draw the player
+function drawPlayer() {
+    player.style.left = currentPosition[0] + 'px'
+    player.style.bottom = currentPosition[1] + 'px'
+}
+
+//move player
+function movePlayer(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawPlayer()
+            }
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] +=10
+                drawPlayer()
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', movePlayer)
