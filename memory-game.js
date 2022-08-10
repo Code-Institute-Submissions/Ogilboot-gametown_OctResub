be732a42c221b76aae2a1a5812328f7714537aab
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-10 13:30:19 +0000

    feat: memory game functioning correctly

--- memory-game.js
+++ memory-game.js
@@ -2,51 +2,51 @@
 const cardArray = [
     {
         name: 'fries',
-        img: 'images/fries.png',
+        img: 'assets/images/fries.png',
     },
     {
         name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
+        img: 'assets/images/cheeseburger.png'
     },
     {
         name: 'hotdog',
-        img: 'images/hotdog.png',
+        img: 'assets/images/hotdog.png',
     },
     {
         name: 'ice-cream',
-        img: 'images/ice-cream.png',
+        img: 'assets/images/ice-cream.png',
     },
     {
         name: 'milkshake',
-        img: 'images/milkshake.png',
+        img: 'assets/images/milkshake.png',
     },
     {
         name: 'pizza',
-        img: 'images/pizza.png',
+        img: 'assets/images/pizza.png',
     },
     {
         name: 'fries',
-        img: 'images/fries.png',
+        img: 'assets/images/fries.png',
     },
     {
         name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
+        img: 'assets/images/cheeseburger.png'
     },
     {
         name: 'hotdog',
-        img: 'images/hotdog.png',
+        img: 'assets/images/hotdog.png',
     },
     {
         name: 'ice-cream',
-        img: 'images/ice-cream.png',
+        img: 'assets/images/ice-cream.png',
     },
     {
         name: 'milkshake',
-        img: 'images/milkshake.png',
+        img: 'assets/images/milkshake.png',
     },
     {
         name: 'pizza',
-        img: 'images/pizza.png',
+        img: 'assets/images/pizza.png',
     },
 ]
 
@@ -55,7 +55,7 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 // variables & arrays
-const gridDisplay = document.querySelector('#grid')
+const gridDisplay = document.querySelector('#memoryGrid')
 const resultDisplay = document.querySelector('#result')
 let cardsChosen = []
 let cardsChosenIds = []
@@ -66,7 +66,7 @@ function createBoard() {
 
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('src', 'assets/images/blank.png')
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         gridDisplay.append(card)
@@ -78,29 +78,29 @@ createBoard()
 // checks for correct / incorrect choices and alerts appropriately
 function checkMatch() {
 
-    const cards = document.querySelectorAll('#grid img')
+    const cards = document.querySelectorAll('#memoryGrid img')
     const optionOneId = cardsChosenIds[0]
     const optionTwoId = cardsChosenIds[1]
 
     if (optionOneId == optionTwoId) 
     {
-        cards[optionOneId].setAttribute('src', 'images/blank.png')
-        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        cards[optionOneId].setAttribute('src', 'assets/images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'assets/images/blank.png')
         alert('You have clicked the same image!')
     }
     if (cardsChosen[0] == cardsChosen[1]) 
     {
         alert('You found a match!')
-        cards[optionOneId].setAttribute('src', 'images/white.png')
-        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].setAttribute('src', 'assets/images/white.png')
+        cards[optionTwoId].setAttribute('src', 'assets/images/white.png')
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard)
         cardsWon.push(cardsChosen)
     } 
     else 
     {
-        cards[optionOneId].setAttribute('src', 'images/blank.png')
-        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        cards[optionOneId].setAttribute('src', 'assets/images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'assets/images/blank.png')
         alert('Sorry try again!')
     }
 
