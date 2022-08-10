48a8695eb3a0a0a751115f0d4c7dfcb58ac27ade
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-10 13:25:43 +0000

    feat: added memory game and assets

---
+++ memory-game.js
@@ -0,0 +1,138 @@
+// list of all our 'cards'
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+]
+
+
+// randomises the card selection when building our grid
+cardArray.sort(() => 0.5 - Math.random())
+
+// variables & arrays
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+// creates our board, populated with 'blank' cards
+function createBoard() {
+
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.append(card)
+
+    }
+}
+createBoard()
+
+// checks for correct / incorrect choices and alerts appropriately
+function checkMatch() {
+
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+
+    if (optionOneId == optionTwoId) 
+    {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image!')
+    }
+    if (cardsChosen[0] == cardsChosen[1]) 
+    {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } 
+    else 
+    {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry try again!')
+    }
+
+    // updates score and resets cards
+    resultDisplay.innerHTML = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    // checks if game complete - if so, reloads page
+    if (cardsWon.length == cardArray.length/2) 
+    {
+        resultDisplay.innerHTML = 'Congratulations! You found them all you Clever Sausage.'
+        setTimeout(reloadGame, 3000)
+    }
+
+}
+
+// 'flips' card
+function flipCard() 
+{
+    const cardId = this.getAttribute('data-id')
+
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) 
+    {
+        setTimeout(checkMatch, 500)
+    }
+}
+
+// reloads page
+function reloadGame() {
+    document.location.reload()
+}
